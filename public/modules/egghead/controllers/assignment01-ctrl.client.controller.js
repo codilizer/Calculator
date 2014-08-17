9db9dbd9ebbdfa7b702f8abdec70e10042c03ec9
codilizer <codilizer@aol.com>
2014-08-17 20:11:19 +0900

    linked bookmarks with mainCtrl controller data
    https://egghead.io/lessons/angularjs-building-an-angular-app-controllers
    https://github.com/eggheadio/egghead-angularjs-from-scratch-getting-started/blob/02-controllers/index.start.html

--- public/modules/egghead/controllers/assignment01-ctrl.client.controller.js
+++ public/modules/egghead/controllers/assignment01-ctrl.client.controller.js
@@ -4,5 +4,6 @@ angular.module('egghead').controller('Assignment01CtrlController', ['$scope',
 	function($scope) {
 		// Assignment01 ctrl controller logic
 		// ...
+        $scope.hello2 = "world2"
 	}
 ]);
