47f17ddc5f7013df4cc4c93d561ff832daf0dbfd
codilizer <codilizer@aol.com>
2014-08-17 20:13:32 +0900

    added controllers/eggly-app.start.js

---
+++ public/modules/egghead/controllers/eggly-app.start.js
@@ -0,0 +1,34 @@
+/**
+ * Created by crunch on 2014-08-17.
+ */
+'use strict';
+// Loading failed
+// angular.module('egghead', [])
+//    .controller('MainCtrl', function ($scope) {
+
+angular.module('egghead').controller('mainCtrl', ['$scope',
+            function($scope) {
+                // Assignment01 ctrl controller logic
+                // ...
+
+            $scope.hello = "world"
+
+            $scope.categories = [
+                {"id": 0, "name": "Development"},
+                {"id": 1, "name": "Design"},
+                {"id": 2, "name": "Exercise"},
+                {"id": 3, "name": "Humor"}
+            ]
+
+            $scope.bookmarks = [
+                {"id": 0, "title": "AngularJS", "url": "http://angularjs.org", "category": "Development" },
+                {"id": 1, "title": "Egghead.io", "url": "http://angularjs.org", "category": "Development" },
+                {"id": 2, "title": "A List Apart", "url": "http://alistapart.com/", "category": "Design" },
+                {"id": 3, "title": "One Page Love", "url": "http://onepagelove.com/", "category": "Design" },
+                {"id": 4, "title": "MobilityWOD", "url": "http://www.mobilitywod.com/", "category": "Exercise" },
+                {"id": 5, "title": "Robb Wolf", "url": "http://robbwolf.com/", "category": "Exercise" },
+                {"id": 6, "title": "Senor Gif", "url": "http://memebase.cheezburger.com/senorgif", "category": "Humor" },
+                {"id": 7, "title": "Wimp", "url": "http://wimp.com", "category": "Humor" },
+                {"id": 8, "title": "Dump", "url": "http://dump.com", "category": "Humor" }
+            ]
+    }]);
