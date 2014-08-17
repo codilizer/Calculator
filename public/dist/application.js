3ad3bb61215a3ddf30aeacbd6474653908149035
codilizer <codilizer@aol.com>
2014-08-17 23:32:40 +0900

    Added functions of editing & creating buttons build min application
    https://egghead.io/lessons/angularjs-building-an-angular-app-simple-states
    
    https://github.com/eggheadio/egghead-angularjs-from-scratch-getting-started/tree/04-simple-states

--- public/dist/application.js
+++ public/dist/application.js
@@ -532,12 +532,43 @@ angular.module('egghead').controller('mainCtrl', [
     $scope.currentCategory = null;
     function setCurrentCategory(category) {
       $scope.currentCategory = category;
+      cancelCreating();
+      cancelEditing();
     }
     function isCurrentCategory(category) {
       return $scope.currentCategory !== null && category.name === $scope.currentCategory.name;
     }
     $scope.setCurrentCategory = setCurrentCategory;
     $scope.isCurrentCategory = isCurrentCategory;
+    // CREATING AND EDITING STATES
+    $scope.isCreating = false;
+    $scope.isEditing = false;
+    function startCreating() {
+      $scope.isCreating = true;
+      $scope.isEditing = false;
+    }
+    function cancelCreating() {
+      $scope.isCreating = false;
+    }
+    function startEditing() {
+      $scope.isCreating = false;
+      $scope.isEditing = true;
+    }
+    function cancelEditing() {
+      $scope.isEditing = false;
+    }
+    function shouldShowCreating() {
+      return $scope.currentCategory && !$scope.isEditing;
+    }
+    function shouldShowEditing() {
+      return $scope.isEditing && !$scope.isCreating;
+    }
+    $scope.startCreating = startCreating;
+    $scope.cancelCreating = cancelCreating;
+    $scope.startEditing = startEditing;
+    $scope.cancelEditing = cancelEditing;
+    $scope.shouldShowCreating = shouldShowCreating;
+    $scope.shouldShowEditing = shouldShowEditing;
   }
 ]);'use strict';
 // Config HTTP Error Handling
